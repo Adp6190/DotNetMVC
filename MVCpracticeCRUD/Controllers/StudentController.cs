1cf80abafebfaf7656485277edf5b297bcd7c348
Adp6190 <130762564+Adp6190@users.noreply.github.com>
2023-09-03 16:11:19 +0530

    COMMITTED

---
+++ MVCpracticeCRUD/Controllers/StudentController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using MVCpracticeCRUD.Models;
+
+namespace MVCpracticeCRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            DbOperations db = new DbOperations();
+            List<Student>stud = db.GetAllStudents();
+            return View(stud);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            DbOperations db =new DbOperations();
+            
+            Student s=db.findStudent(id);
+            return View(s);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+
+                Student s=new Student();
+                DbOperations db=new DbOperations();
+                s.Id = Convert.ToInt32(collection["Id"]);
+                s.Name = collection["Name"].ToString();
+                s.Department = collection["Department"].ToString();
+                db.InsertStudent(s);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int Id)
+        {
+            DbOperations db=new DbOperations();
+            Student s = db.findStudent(Id);
+            return View(s);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int Id, Student s)
+        {
+            try
+            {
+                DbOperations db = new DbOperations();
+                db.UpdateStudent(Id, s);       
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            DbOperations db=new DbOperations(); 
+            Student stud=db.findStudent(id);
+            return View(stud);
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                DbOperations db=new DbOperations();
+                db.DeleteStudent(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCpracticeCRUD/DbOperations.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Data.SqlClient;
+using MVCpracticeCRUD.Models;
+using System.Data;
+
+
+namespace MVCpracticeCRUD
+{
+    public class DbOperations
+    {
+
+        public void InsertStudent(Student s)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DB1;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Insert into Student values(@Id,@Name,@Department)",cn);
+            cn.Open();
+            cmd.Parameters.AddWithValue("Id", s.Id);
+            cmd.Parameters.AddWithValue("Name", s.Name);
+            cmd.Parameters.AddWithValue("Department", s.Department);
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+        }
+
+        public void UpdateStudent(int Id, Student s)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DB1;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("UPDATE  Student set Name=@Name, Department=@Department where Id=" + Id, cn);
+            cn.Open();
+            cmd.Parameters.AddWithValue("Name", s.Name);
+            cmd.Parameters.AddWithValue("Department", s.Department);
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+        }
+
+        public void DeleteStudent(int Id)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DB1;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Delete from Student where Id=" + Id, cn);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+        }
+
+        public Student findStudent(int Id)
+        {
+            SqlConnection cn = new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = DB1; Integrated Security = True");
+            SqlCommand cmd = new SqlCommand("select * from Student where Id=" + Id, cn);
+            cn.Open();
+            Student s = new Student();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                s.Id = (int)dr[0];
+                s.Name = (string)dr[1];
+                s.Department = (string)dr[2];
+            }
+            cn.Close();
+            cn.Dispose();
+            return s;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            SqlConnection cn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DB1;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select * from Student",cn);
+            cn.Open();
+            List<Student> stud = new List<Student>();
+            SqlDataReader datareader = cmd.ExecuteReader();
+            DataTable datatable = new DataTable();
+            datatable.Load(datareader);
+            for (int i = 0; i < datatable.Rows.Count; i++)
+            {
+                Student s = new Student();
+                s.Id = (int)datatable.Rows[i][0];
+                s.Name = (string)datatable.Rows[i][1];
+                s.Department = (string)datatable.Rows[i][2];
+                stud.Add(s);
+            }
+            Console.WriteLine("List is");
+            for (int i = 0; i < stud.Count; i++)
+            {
+                Console.WriteLine(stud[i].Id + " " + stud[i] + " " + stud[i].Department);
+            }
+            cn.Close();
+            cn.Dispose();
+            return stud;
+
+
+        }
+    }
+}
